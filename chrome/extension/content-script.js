e986c65ab72ed1570112b0040a743ae9fe5bc4a6
Philip Khonin <philip.khonin@flightnetwork.com>
2017-06-23 16:49:28 -0400

    moved the logic to content-script.js

--- chrome/extension/content-script.js
+++ chrome/extension/content-script.js
@@ -1,9 +1,64 @@
-const sillyBella = () => {
-  console.log("screw you Bella");
+const fieldNames = [
+  'email',
+  'reTypeEmail',
+  'dayTimeNumber',
+  'title',
+  'dateOfBirth.year',
+  'dateOfBirth.month',
+  'dateOfBirth.day',
+  'firstName',
+  'middleNames',
+  'lastName',
+  'location.country',
+  'location.region',
+  'specialRequests.frequentFlyerProgram',
+  'specialRequests.mileageProgramNumber',
+  'specialRequests.specialRequirements',
+  'selectedPackage',
+  // 'bpg', // doesn't have a name
+  'paymentSource.ccNumber',
+  'paymentSource.securityCode',
+  'accountHolder.firstName',
+  'accountHolder.lastName',
+  'paymentSource.expiryMonth',
+  'paymentSource.expiryYear',
+  'billingAddress.country',
+  'billingAddress.street',
+  'billingAddress.unit',
+  'billingAddress.city',
+  'billingAddress.region',
+  'billingAddress.postalCode',
+];
+
+const getFields = () => (
+  fieldNames.map(
+    field => ({
+      name: field,
+      elements: document.getElementsByName(field),
+    })
+  )
+);
+
+const fillFieldsOfSameType = ({ elements, name }) => (
+  elements.forEach((element, index) => {
+    element.value = data[name] || 'test';
+  })
+);
+
+const fillFields = () => (
+  getFields().forEach((field) => {
+    fillFieldsOfSameType(field);
+  })
+);
+
+const data = {
+  firstName: 'Roofus',
+  middleNames: 'Moofus',
+  lastName: 'Summers',
 };
 
 chrome.runtime.sendMessage('getOptions', (response) => {
-  if (!window.formFiller) {
-    window.formFiller = sillyBella();
+  if (window && !window.formFiller) {
+    window.formFiller = fillFields;
   }
 });
